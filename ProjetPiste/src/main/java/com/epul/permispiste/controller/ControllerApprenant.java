10ea6e341a59b89bc1afb40629fb2d5f395c42c2
Clémence <98712982+clemenceprv@users.noreply.github.com>
2023-06-25 09:32:58 +0100

    Contrôleur => API ControllerApprenant.java

--- ProjetPiste/src/main/java/com/epul/permispiste/controller/ControllerApprenant.java
+++ ProjetPiste/src/main/java/com/epul/permispiste/controller/ControllerApprenant.java
@@ -1,3 +1,77 @@
+package com.epul.permispiste.controller;
+import com.epul.permispiste.domains.ApprenantEntity;
+import com.epul.permispiste.dto.ApprenantDTO;
+import com.epul.permispiste.mesExceptions.MonException;
+import com.epul.permispiste.service.ApprenantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+
+@RequestMapping("/apprenant")
+@RestController
+@CrossOrigin
+public class ControllerApprenant {
+
+    @Autowired
+    private ApprenantService apprenantService;
+
+    private HttpSession session;
+
+    @GetMapping("/getAll/")
+    public List<ApprenantDTO> getAllAction(HttpServletRequest request) {
+        List<ApprenantDTO> apprenants = null;
+        try
+        {
+            apprenants = apprenantService.getAll();
+        }
+        catch (MonException e) {
+            ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            ResponseEntity.notFound().build();
+        }
+        return apprenants;
+    }
+
+    @GetMapping("/addForm")
+    public ResponseEntity<?> addForm(HttpServletRequest request) {
+        try {
+            HttpSession session = request.getSession();
+            if (session.getAttribute("id") == null) {
+                String message = "Vous n'êtes pas connecté !!";
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+            } else {
+                return ResponseEntity.ok("vues/apprenant/ajouterApprenant");
+            }
+        } catch (Exception e) {
+            String errorMessage = "Une erreur s'est produite lors de l'ajout de l'apprenant";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addForm(HttpServletRequest request, @RequestBody ApprenantEntity apprenantData) {
+        try {
+            HttpSession session = request.getSession();
+//            if (session.getAttribute("id") == null) {
+//                String message = "Vous n'êtes pas connecté !!";
+//                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(message);
+//            } else {
+            // TODO : Décommenter pour gérer la connexion
+            apprenantService.addApprenant(apprenantData);
+            return ResponseEntity.ok("Action ajoutée avec succès");
+//            }
+        } catch (Exception e) {
+            String errorMessage = "Une erreur s'est produite lors de l'ajout de l'action";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+}
+
 //package com.epul.permispiste.controller;
 //import com.epul.permispiste.domains.ApprenantEntity;
 //import com.epul.permispiste.domains.UtilisateurEntity;
